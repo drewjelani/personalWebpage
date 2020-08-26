86d38014e01ec42d55df51b8bf2663fa866e1d52
Drew Dickens <anddica@amazon.com>
2020-08-25 17:20:45 -0700

    Moving page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "../components/header";
+import images from "../components/images";
 
 const About = () => {
     return (
         <div>
             <Header/>
-            <div className='container'>
-                <p>About</p>
+            <div className="container">
+                <span style={{"textAlign":"center", "display":"block"}}>
+                    <img src={images.DrewUpperHalfFromEngagementShoot} className="rounded center" height="60%" width="60%" alt="Drew Upper Engagement"/>
+                </span>
             </div>
         </div>
     )
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Header from "../components/header";
-import images from "../components/images";
 
 const Home = () => {
     return (
         <div>
             <Header/>
-            <div className="container">
-                <span style={{"textAlign":"center", "display":"block"}}>
-                    <img src={images.DrewUpperHalfFromEngagementShoot} className="rounded center" height="60%" width="60%" alt="Drew Upper Engagement"/>
-                </span>
-                <p>
-                    This is the start of Drew's page.  Coming soon!
-                </p>
-            </div>
+                <div className={'container'}>
+                    <p>
+                        This is the start of Drew's page.  Coming soon!
+                    </p>
+                </div>
         </div>
     )
 };
